a7b977622cc9b9fa9f48cfb415bd6df7a9de2094
Tim Schwartz <tim@metaverse.systems>
2025-05-04 05:12:05 -0500

    chore: Update @clarion-app/types to version 1.3.0 and refactor backend initialization

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,14 @@
 import { BackendType } from "@clarion-app/types";
 
-export const backend: BackendType = { url: "http://localhost:8000", token: "" };
+export const backend: BackendType = { url: "http://localhost:8000", token: "", user: { id: "", name: "", email: ""} };
 
-export const initializeFrontend = (setBackendUrl: string) => {
-    backend.url = setBackendUrl;
-};
-
-export const setFrontendToken = (token: string) => {
-    backend.token = token;
+export const updateFrontend = (config: BackendType) => {
+    backend.url = config.url;
+    backend.token = config.token;
+    backend.user = config.user;
 };
 
 export { entryApi } from './entryApi';
 export { locationApi } from './locationApi';
 export { healthMetricApi } from './healthMetricApi';
-
 export { Entries } from './Entries';
